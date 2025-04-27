476f414d8ded026ab65d9c6977cd186316e456ba
Ryan9056 <Wagner6@mail.uc.edu>
2025-04-27 15:47:44 -0400

    Game Logic Start and Random CPU

--- webpage/MainScene.js
+++ webpage/MainScene.js
@@ -1,13 +1,15 @@
 // view of the Computer's grid
-class ComputerView extends Phaser.Scene {
+class MainScene extends Phaser.Scene {
     constructor() {
         super("playGame");
         this.CPUboard = Array.from({ length: 10 }, () => Array(10).fill(0));;
-        this.Playerborad = Array.from({ length: 10 }, () => Array(10).fill(0));;
+        this.Playerboard = Array.from({ length: 10 }, () => Array(10).fill(0));;
+        this.Playerattemptboard = Array.from({ length: 10 }, () => Array(10).fill(0));
+        this.CPUattemptboard = Array.from({ length: 10 }, () => Array(10).fill(0));
     }
 
     preload() {
-        this.load.image("ocean", "assets/ocean.avif");
+        this.load.image("ocean", "assets/ocean.png");
         this.load.image("ship", "assets/ship.png");
 
 
@@ -19,16 +21,14 @@ class ComputerView extends Phaser.Scene {
         this.CPUboard = this.generateBoards();
         this.Playerboard = this.generateBoards();
 
-        for (let row = 0; row < 10; row++) {
-            for (let col = 0; col < 10; col++) {
-                console.log(this.CPUboard[row][col]);
-            }
-        }
 
-        for (let row = 0; row < 10; row++) {
-            for (let col = 0; col < 10; col++) {
-                console.log(this.Playerboard[row][col]);
+        // remove when complete
+        for (let row = 0; row < this.CPUboard.length; row++) {
+            let rowString = '';
+            for (let col = 0; col < this.CPUboard[row].length; col++) {
+                rowString += this.CPUboard[row][col] + ' '; 
             }
+            console.log(rowString); 
         }
 
 
@@ -48,6 +48,7 @@ class ComputerView extends Phaser.Scene {
 
                 const wave = this.add.image(x, y, "ocean");
 
+                // remove when complete
                 if (this.CPUboard[row][col] == 1) {
                     wave.setTexture('ship');
                 }
@@ -57,7 +58,7 @@ class ComputerView extends Phaser.Scene {
                 // Center the wave image in the cell
                 wave.setOrigin(0)
                     .setInteractive()
-                    .on('pointerdown', () => this.getGridIndex(row, col))
+                    .on('pointerdown', () => this.onClick(row, col))
                     .on('pointerover', () => this.enterButtonHoverState(wave))
                     .on('pointerout', () => this.enterButtonRestState(wave));
 
@@ -97,17 +98,25 @@ class ComputerView extends Phaser.Scene {
 
     }
 
-    getGridIndex(x, y) {
-        console.log(x + " " + y)
+    onClick(x, y) {
+        console.log(x + " " + y);
+        if (this.Playerattemptboard[x][y] != 1 && this.Playerattemptboard[x][y] != 2) {
+            this.gameChecks(x, y);
+            this.CPUmove();
+        } else {
+            //can't choose option
+            console.log("can't choose option");
+        }
+
     }
 
     enterButtonHoverState(wave) {
-        wave.clearTint()
-        wave.setTintFill("#8d8e8f")
+        wave.clearTint();
+        wave.setTintFill("0x000000");
     }
 
     enterButtonRestState(wave) {
-        wave.clearTint()
+        wave.clearTint();
     }
 
     generateBoards() {
@@ -162,6 +171,53 @@ class ComputerView extends Phaser.Scene {
 
         return board;
     }
+
+    gameChecks(x, y) {
+        if (this.CPUboard[x][y] == 0) {
+            this.Playerattemptboard[x][y] = 1;
+            console.log("miss");
+        } else if (this.CPUboard[x][y] == 1)
+        {
+            this.Playerattemptboard[x][y] = 2;
+            console.log("hit");
+            if(this.Playerattemptboard.flat().filter(x => x ===2).length >= 17) {
+                //win 
+                console.log("win");
+            }
+        } 
+
+        
+
+        
+
+    }
+
+
+
+    CPUmove() {
+        let x = Math.floor(Math.random() * 10);
+        let y = Math.floor(Math.random() * 10);
+
+        while (this.CPUattemptboard[x][y] === 1 || this.CPUattemptboard[x][y] === 2) {
+            x = Math.floor(Math.random() * 10);
+            y = Math.floor(Math.random() * 10);
+        }
+
+        if (this.Playerboard[x][y] == 0) {
+            this.CPUattemptboard[x][y] = 1;
+            console.log("CPU miss");
+        } else if (this.Playerboard[x][y] == 1)
+        {
+            this.CPUattemptboard[x][y] = 2;
+            console.log("CPU hit");
+            if(this.CPUattemptboard.flat().filter(x => x ===2).length >= 17) {
+                //lose
+                console.log("lose");
+            }
+        } 
+        console.log(x + " " + y);
+
+    }
 }
 
 
@@ -171,11 +227,13 @@ class ComputerView extends Phaser.Scene {
 
 
 
+
+
 new Phaser.Game({
-    width: 500,
-    height: 800,
-    backgroundColor: 0x000000,
-    scene: ComputerView,
+    width: 400,
+    height: 720,
+    backgroundColor: 0xffffff,
+    scene: MainScene,
     physics: { default: 'arcade' },
     parent: 'game'
 });
