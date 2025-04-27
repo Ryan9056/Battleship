d217f78a1d037073d1561b196e578ba692649d8f
Ryan9056 <Wagner6@mail.uc.edu>
2025-04-26 20:42:02 -0400

    Screen and buttons

--- webpage/ComputerView.js
+++ webpage/ComputerView.js
@@ -2,7 +2,7 @@
 class ComputerView extends Phaser.Scene {
     constructor() {
         super("playGame");
-        this.computerGrid = new grid();
+        this.gameBoard = [];
     }
 
     preload() {
@@ -15,26 +15,46 @@ class ComputerView extends Phaser.Scene {
         const cellSize = 32;
     
     
-        const gridArray = computerGrid.getGrid();
+        const CPUarray = Array.from({ length: 10 }, () => Array(10).fill(0));
     
         // Iterate over the grid and assign the wave image to each cell
-        for (let row = 0; row < gridArray.length; row++) {
-            for (let col = 0; col < gridArray[row].length; col++) {
+        for (let row = 0; row < 10; row++) {
+            for (let col = 0; col < 10; col++) {
                 // Add the wave image to the scene at the correct position
                 const x = col * cellSize;
                 const y = row * cellSize;
                 const wave = this.add.image(x, y, "ocean");
     
                 // Center the wave image in the cell
+                wave.setOrigin(0)
+                    .setInteractive()
+                    .on('pointerdown', () => this.getGridIndex(row, col) )
+                    .on('pointerover', () => this.enterButtonHoverState(wave) )
+                    .on('pointerout', () => this.enterButtonRestState(wave) );
+                
+    
+                // Store the wave image in the grid
+                CPUarray[row][col] = wave;
+            }
+        }
+
+
+        const Playerarray = Array.from({ length: 10 }, () => Array(10).fill(0));
+
+        for (let row = 0; row < 10; row++) {
+            for (let col = 0; col < 10; col++) {
+                // Add the wave image to the scene at the correct position
+                const x = col * cellSize;
+                const y = row * cellSize + 400;
+                const wave = this.add.image(x, y, "ocean");
+    
+                // Center the wave image in the cell
                 wave.setOrigin(0);
     
                 // Store the wave image in the grid
-                gridArray[row][col] = wave;
+                Playerarray[row][col] = wave;
             }
         }
-    
-        // Update the grid in the grid class
-        computerGrid.setGrid(gridArray);
     
         
     }
@@ -43,4 +63,28 @@ class ComputerView extends Phaser.Scene {
 
     }
 
+    getGridIndex(x, y) {
+        console.log(x + " " + y)
+    }
+
+    enterButtonHoverState(wave) {
+        wave.clearTint()
+        wave.setTintFill("#8d8e8f")
+    }
+
+    enterButtonRestState(wave) {
+        wave.clearTint()
+    }
+
+
+
 }
+
+new Phaser.Game({
+    width: 500,
+    height: 800,
+    backgroundColor: 0x000000,
+    scene: ComputerView,
+    physics: { default: 'arcade' }, 
+    parent: 'game'
+  });
